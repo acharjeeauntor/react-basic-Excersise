70ff40c751c8b7af489c3f4fee71c55a7e1ad647
acharjeeauntor <acharjeeauntor@gmail.com>
2019-06-06 16:59:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ state = {
+    persons:[
+      {id:'a1',name:'Auntor',age:20},
+      {id:'a2',name:'ridoy',age:27},
+      {id:'a3',name:'Ontu',age:25}
+    ],
+    otherState:'new state',
+    showPersons:false
+  }
+nameChangeHandler=(event,id)=>{
+const personIndex = this.state.persons.findIndex(p=>{
+  return p.id===id
+})
+
+const person = {
+  ...this.state.persons[personIndex]
 }
 
-export default App;
+person.name = event.target.value
+const persons = [...this.state.persons]
+persons[personIndex] = person
+this.setState({persons:persons})
+}
+
+deletePersonHandler=(personIndex)=>{
+//const persons = this.state.persons
+const persons = [...this.state.persons]
+persons.splice(personIndex,1)
+this.setState({persons:persons})
+}
+
+togglePersonsHandler=()=>{
+const doesShow = this.state.showPersons
+this.setState({showPersons:!doesShow})
+}
+
+
+
+
+  render(){
+    const style = {
+      backgroundColor: 'green',
+      color:'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons =(
+        <div>
+          {this.state.persons.map((person,index)=>{
+            return <Person
+            click={()=>this.deletePersonHandler(index)} 
+            name={person.name} 
+            age={person.age} 
+            key={person.id}
+            changed={(event)=>this.nameChangeHandler(event,person.id)}
+            />
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red'
+    }
+
+   const classes =[]
+   if(this.state.persons.length<=2){
+     classes.push('red')
+   }
+   if(this.state.persons.length<=1){
+     classes.push('bold')
+   }
+
+
+  return (
+    <div className="App">
+     <h1> Hi, I am Auntor </h1>
+     <p className={classes.join(' ')}> its react app</p>
+     <button
+     style={style}
+     onClick={this.togglePersonsHandler}> Toggle Persons </button>
+     {persons}
+    </div>
+  )
+}
+}
+
+export default App
